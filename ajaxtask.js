de6fd7fe4006c1315071074826055d702bbd2c1f
Kashish Mittal <kashkind7@gmail.com>
2019-04-17 00:19:55 +0530

    firstTask

---
+++ ajaxtask.js
@@ -0,0 +1,17 @@
+$(function (){
+    $('#show').click(function (){
+        var user=$('#uname').val();
+        $.ajax({
+            type: 'GET',
+            url: 'http://api.github.com/users/'+user+'/followers',
+            success: function (data) {
+                $.each(data,function(i, ulogin){
+                    $('#unames').append('<li>'+ ulogin.login+ '</li>');
+                })
+            
+            }
+        })
+    })
+
+});
+

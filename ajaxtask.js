1a4ba3d1828c8588c6f188d520b1b2db912e025c
Kashish Mittal <45570942+KASHISH2000@users.noreply.github.com>
2019-04-17 00:46:13 +0530

    Update ajaxtask.js

--- ajaxtask.js
+++ ajaxtask.js
@@ -3,7 +3,7 @@ $(function (){
         var user=$('#uname').val();
         $.ajax({
             type: 'GET',
-            url: 'http://api.github.com/users/'+user+'/followers',
+            url: 'https://api.github.com/users/'+user+'/followers',
             success: function (data) {
                 $.each(data,function(i, ulogin){
                     $('#unames').append('<li>'+ ulogin.login+ '</li>');

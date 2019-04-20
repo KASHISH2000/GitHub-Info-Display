49ac6df60245f5eda5b030d2abf8d49308ec782f
Kashish Mittal <kashkind7@gmail.com>
2019-04-20 09:11:14 +0530

    features addedd

--- ajaxtask.js
+++ ajaxtask.js
@@ -1,17 +1,28 @@
-$(function (){
-    $('#show').click(function (){
-        var user=$('#uname').val();
+$(function () {
+    function callajax(name) {
+        $('.loader').show()
         $.ajax({
             type: 'GET',
-            url: 'https://api.github.com/users/'+user+'/followers',
+            url: 'https://api.github.com/users/' + name + '/followers',
             success: function (data) {
-                $.each(data,function(i, ulogin){
-                    $('#unames').append('<li>'+ ulogin.login+ '</li>');
+                $('.loader').hide()
+                $.each(data, function (i, ulogin) {
+                    $('#unames').append('<li>' + ulogin.login + '</li>')
                 })
-            
             }
         })
+    }
+    $('#show').click(function () {// show is id of the show button
+        var user = $('#uname').val();//uname is id of textfield in which we take user input
+        callajax(user)
+    }),
+    $('.list').click(function(e){ // list is the class of ol tag
+        var kname=e.target.innerHTML;
+        $('#unames').empty();
+        callajax(kname)
+    })
+    $('#uname').focus(function () {
+        $('#unames').empty();
     })
 
-});
-
+    })
